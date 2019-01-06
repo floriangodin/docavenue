42ebe0c8ecdfef708a83f3f43a6a6aa0de06c74e
florian godin <florian@debian.com>
2019-01-07 00:02:54 +0100

    composant

--- src/App.js
+++ src/App.js
@@ -1,26 +1,95 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import JwPagination from 'jw-react-pagination';
+
 import './App.css';
+import styled from 'styled-components';
+
+
+
+
+const StyledDiv = styled.div`
+  width:100%;
+  height:auto;
+  margin:auto;
+  background-color: rgb(238,238,238);
+`;
+const StyledTitle = styled.h1`
+font-size:2rem;
+text-align: center;
+padding-top: 10px;
+margin-top:0;
+font-style: italic;
+`;
+
+const StyledPhoto = styled.li`
+display: inline-block;
+height: auto;
+width: 18%;
+padding:10px;
+
+  > img{
+    width:100%;
+    height:100%;
+  }
+`;
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      isLoading: true,
+      datas: [],
+      pageOfItems:[],
+      error: null,
+      activePage: 1,
+      itemPerPage:15,
+    };
+  }
+
+  fetchData= () => {
+    fetch(`https://jsonplaceholder.typicode.com/photos`)
+    .then(response => response.json())
+    .then(data =>
+      this.setState({
+        datas: data,
+        isLoading: false,
+      })
+    )
+    .catch(error => this.setState({error:error, isLoading:false}));
+  };
+
+  handlePageChange = (pageOfItems) => {
+    this.setState({
+      pageOfItems
+    });
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
   render() {
+    const {isLoading, datas, error} = this.state;
+
+    /*var tof = this.state.datas.sort((a, b) => a.title.localeCompare(b.title)).map(image => (
+      <StyledPhoto key={image.id}><p>{image.title}</p></StyledPhoto>
+    )
+  );*/
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <StyledDiv>
+        <StyledTitle>5000 nuances de couleurs</StyledTitle>
+        {error ? <p>{error.message}</p> : null}
+
+        {!isLoading ? this.state.pageOfItems.sort((a, b) => a.title.localeCompare(b.title)).map(image => (
+            <StyledPhoto key={image.id}><img src={image.url} alt={image.title}/></StyledPhoto>
+          )
+        ): (
+        <p>...isLoading</p>
+      )
+          }
+          <JwPagination items= {datas} onChangePage={this.handlePageChange} pageSize= {15}/>
+
+      </StyledDiv>
     );
   }
 }
